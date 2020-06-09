388be98daaca8f8ed6eeb09345e05e3e3314c996
Aleksey Shuklin <aleksey.shuklin@singularis-lab.com>
2020-06-09 15:56:25 +0400

    fix enum

--- api/table/enums/EObjectType.py
+++ api/table/enums/EObjectType.py
@@ -4,8 +4,8 @@ from django.db import models
 class EObjectType(models.IntegerChoices):
     TREE = 0
     LIGHT = 1
-    ADVERTISMENT = 3
-    MAILBOX = 4
-    BUS_STOP = 5
-    SIGN = 6
-    BENCH = 7
+    ADVERTISMENT = 2
+    MAILBOX = 3
+    BUS_STOP = 4
+    SIGN = 5
+    BENCH = 6
